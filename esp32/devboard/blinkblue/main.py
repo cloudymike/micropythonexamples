58f5a490c94d2577fd166b3418de072c9a64cf2c
cloudymike <mikael@cloudyforecast.com>
2020-09-15 18:59:48 -0700

    Back in the game again

--- esp32/devboard/blinkblue/main.py
+++ esp32/devboard/blinkblue/main.py
@@ -1,17 +1,15 @@
 import machine
 import time
 
-blueLed = machine.Pin(2, machine.Pin.OUT)
-pin = machine.Pin(18)
+def blinkblue(ledPin=2):
+    blueLed = machine.Pin(ledPin, machine.Pin.OUT)
+    pin = machine.Pin(18)
 
-while(1):
-    blueLed.off()
-    time.sleep_ms(500)
-    blueLed.on()
-    time.sleep_ms(500)
+    while(1):
+        blueLed.off()
+        time.sleep_ms(500)
+        blueLed.on()
+        time.sleep_ms(500)
 
-
-
-
-
-
+if __name__ == "__main__":
+    blinkblue()
