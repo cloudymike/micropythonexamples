ff22b408c810756f223122522b7268acad0d8486
cloudymike <mikael@cloudyforecast.com>
2020-11-18 21:19:33 -0800

    Recapping previous work and updating local mqtt to run easier

--- esp32/devboard/mqtt_pub/loadfiles.sh
+++ esp32/devboard/mqtt_pub/loadfiles.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 PORT='/dev/ttyUSB0'
 PUSHCMD="ampy --port $PORT put "
+
+# Enter your path to your WLAN configuration file here, see ../wlan/wlanconfig.py for example
+$PUSHCMD ~/secrets/wlanconfig.py
+
+# This is just to get the host IP address, you may have to change it
+echo "MQTT_HOST='$(hostname -I | awk '{print $1}')'" >mqtthost.py
+$PUSHCMD mqtthost.py
+
 $PUSHCMD ../wlan/wlan.py
 $PUSHCMD mqtt_writer.py
 $PUSHCMD main.py
-echo "Reset board manually"
+echo "Reset board manually and count to 5, slowly..."
 mosquitto_sub -t sensor-data
