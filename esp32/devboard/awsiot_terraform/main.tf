6030f868544a1994841a1efebe264dd99693f966
cloudymike <mikael@cloudyforecast.com>
2020-12-29 16:46:21 -0800

    wip

--- esp32/devboard/awsiot_terraform/main.tf
+++ esp32/devboard/awsiot_terraform/main.tf
@@ -10,6 +10,15 @@ resource "aws_iot_thing" "upyex_iot_thing" {
 resource "aws_iot_certificate" "upyex_cert" {
   active = true
 }
+# Attach AWS iot cert generated above to the aws iot thing(s)
+resource "aws_iot_thing_principal_attachment" "upyex_principal_att" {
+  principal = aws_iot_certificate.upyex_cert.arn
+  thing     = aws_iot_thing.upyex_iot_thing.name
+}
+
+
+# Output created information for use by awsiot MicroPython Code
+###############################################################
 # Output certificate to /cert/ folder
 resource "local_file" "upyex_cert_pem" {
   content     = aws_iot_certificate.upyex_cert.certificate_pem
@@ -20,6 +29,12 @@ resource "local_file" "upyex_private_key" {
   content     = aws_iot_certificate.upyex_cert.private_key
   filename = "${path.module}/certs/upyex_cert.private.key"
 }
+# Output public key to /cert/ folder
+resource "local_file" "upyex_public_key" {
+  content     = aws_iot_certificate.upyex_cert.public_key
+  filename = "${path.module}/certs/upyex_cert.public.key"
+}
+
 
 
 # Get the aws iot endpoint to print out for reference
