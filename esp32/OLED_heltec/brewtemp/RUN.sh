82da814f89bf29ccff6a2f623bce1ee8ec8ce236
cloudymike <mikael@cloudyforecast.com>
2021-01-19 20:14:22 -0800

    wip

--- esp32/OLED_heltec/brewtemp/RUN.sh
+++ esp32/OLED_heltec/brewtemp/RUN.sh
@@ -7,5 +7,6 @@ TOPDIR=${CURDIR%/*}
 $PUSHCMD $TOPDIR/oled/ssd1306.py
 $PUSHCMD $TOPDIR/textout/textout.py
 $PUSHCMD $TOPDIR/LED/LED.py
+$PUSHCMD tempreader.py
 $PUSHCMD main.py
 sudo timeout 2  ampy --port /dev/ttyUSB0 run ../reset/reset.py
--- esp32/OLED_heltec/brewtemp/main.py
+++ esp32/OLED_heltec/brewtemp/main.py
@@ -1,27 +1,18 @@
-import machine
-import time
-import json
-import ubinascii
-from ds18x20 import DS18X20
-from onewire import OneWire
+import tempreader
 import textout
+import time
 
-pin = machine.Pin(18)
-
-o = OneWire(pin)
-d = DS18X20(o)
 t = textout.textout()
-
+unit='F'
+tempDevice = tempreader.tempreader(unit)
 while(1):
-    devicelist = d.scan()
-    tempdict = {}
-    d.convert_temp()
-
+    temp = tempDevice.get_temp()
+    t.text("Temp: {:.1f}{}".format(temp,unit))
     time.sleep_ms(750)
 
-    # Multiple temperature sensors are OK with onewire bus
-    for device in devicelist:
-        tempdict[ubinascii.hexlify(device)] = d.read_temp(device)
+'''
+    tempdict = tempDevice.get_temp_list()
     for deviceID, temp in tempdict.items():
         print("ID: {}, Temp: {}".format(deviceID, temp))
-        t.text("Temp: {:.1f}C".format(temp))
+        t.text("Temp: {:.1f}{}".format(temp,unit))
+'''
