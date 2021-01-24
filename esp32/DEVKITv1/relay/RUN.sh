2c1dd6a0937595a9e73dad0c9cf4b7ac55e218e4
cloudymike <mikael@cloudyforecast.com>
2021-01-23 22:57:01 -0800

    Relay working on devkit

---
+++ esp32/DEVKITv1/relay/RUN.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PORT='/dev/ttyUSB0'
+PUSHCMD="ampy --port $PORT put "
+
+ampy --port $PORT put  relay.py
+ampy --port $PORT put  main.py
+sudo timeout 2  ampy --port /dev/ttyUSB0 run ../reset/reset.py
---
+++ esp32/DEVKITv1/relay/relay.py
@@ -0,0 +1,4 @@
+import machine
+
+HOT = machine.Pin(18, machine.Pin.OUT)
+COLD = machine.Pin(23, machine.Pin.OUT)
