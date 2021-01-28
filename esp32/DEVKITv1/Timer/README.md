f61ed68040221aa85a2d2fc64baf313dc7cd5a7e
cloudymike <mikael@cloudyforecast.com>
2021-01-27 18:53:30 -0800

    initial

---
+++ esp32/DEVKITv1/Timer/README.md
@@ -0,0 +1,6 @@
+# Machine Timer
+
+Use of RTOS based timers to allow functions to be done in a scheduled way
+outside of a main loop
+
+Also showing how to use a lambda function with parameters to set it all up.
---
+++ esp32/DEVKITv1/Timer/main.py
@@ -0,0 +1,21 @@
+from machine import Timer
+import time
+sum = 0
+
+def cb(x):
+    global sum
+    sum = sum+x
+    print("Timer adder:{}".format(sum))
+
+
+tim = Timer(-1)
+tim.init(period=5000, mode=Timer.ONE_SHOT, callback=lambda t:print("Hello world"))
+
+tim3 = Timer(-1)
+tim3.init(period=3000, mode=Timer.PERIODIC, callback=lambda t:cb(3))
+
+count = 0
+while True:
+    count = count + 1
+    print("            Main loop: {}".format(count))
+    time.sleep(1)
