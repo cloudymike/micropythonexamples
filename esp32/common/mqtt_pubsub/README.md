c181cfe4bd47e425a711a866be8f163503ba972d
cloudymike <mikael@cloudyforecast.com>
2021-01-28 19:21:50 -0800

    Local working

---
+++ esp32/common/mqtt_pubsub/README.md
@@ -0,0 +1,4 @@
+# MQTT locally
+
+This assumes that you have a local mqtt server running on the
+same host as you are developing on.
---
+++ esp32/common/mqtt_pubsub/main.py
@@ -0,0 +1,24 @@
+# Measures magnetic filed with interal hall sensor
+# Use a magnet right over the chip, try to turn it if the number does not change
+# Be careful, you do not want it to find the pins and short them.
+
+import machine
+import esp32
+import time
+import wlan
+from mqtt_local import MQTTlocal
+import mqtthost
+# Do this with an import
+#MQTT_HOST='192.168.62.26'
+wlan.do_connect()
+
+# Should this be esp32?
+m = MQTTlocal('esp8266', mqtthost.MQTT_HOST, 1883, 'sensor-data', 'BlueLED')
+while(1):
+    magneto=esp32.hall_sensor()
+    print(magneto)
+    m.on_next(magneto)
+    time.sleep_ms(2000)
+    m.check_msg()
+
+m.disconnect()
