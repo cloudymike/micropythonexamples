bd6fd45510a62f8552920ad7128a10932c781b27
cloudymike <mikael@cloudyforecast.com>
2021-01-28 19:54:17 -0800

    workingPub

---
+++ esp32/DEVKITv1/awspub2/main.py
@@ -0,0 +1,26 @@
+#AWS MQTT client cert example for esp8266 or esp32 running MicroPython 1.9
+import time
+import wlan
+import json
+import esp32
+import awsiotconfig
+
+from mqtt_writer_aws import MQTTWriterAWS
+
+wlan.do_connect()
+m = MQTTWriterAWS(
+    awsiotconfig.MQTT_CLIENT_ID,
+    awsiotconfig.MQTT_HOST,
+    awsiotconfig.MQTT_PORT,
+    awsiotconfig.MQTT_TOPIC,
+    awsiotconfig.KEY_FILE,
+    awsiotconfig.CERT_FILE)
+while(1):
+    magneto=esp32.hall_sensor()
+    print(magneto)
+    #m.pub_msg("{\"message\":" + str(magneto) + "}")
+    m.on_next(magneto)
+    time.sleep_ms(750)
+    if __name__ != "__main__":  # pragma: no cover
+        print('TESTOK')
+        break
