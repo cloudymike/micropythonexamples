34e41f527f2ddef6c2dfdfea41b1ad1f95e9e4ae
cloudymike <mikael@cloudyforecast.com>
2021-04-01 21:15:13 -0700

    Initial

--- common/gcloud-pub/main.py
+++ common/gcloud-pub/main.py
@@ -14,7 +14,6 @@
 import machine
 import esp32
 from third_party import string
-#import network
 import socket
 import os
 import utime
@@ -31,9 +30,6 @@ import LED
 
 LED.LED.value(1)
 
-def on_message(topic, message):
-    print((topic,message))
-
 
 def set_time():
     ntptime.settime()
@@ -42,49 +38,61 @@ def set_time():
     machine.RTC().datetime(tm)
     print('current time: {}'.format(utime.localtime()))
 
+def on_message(topic, message):
+    print((topic,message))
+
+
 def b42_urlsafe_encode(payload):
     return string.translate(b2a_base64(payload)[:-1].decode('utf-8'),{ ord('+'):'-', ord('/'):'_' })
 
-def create_jwt(project_id, private_key, algorithm, token_ttl):
-    print("Creating JWT...")
-    private_key = rsa.PrivateKey(*private_key)
+class MQTTgcloud:
+    def __init__(self):
+        self.jwt = self.create_jwt(config.google_cloud_config['project_id'], config.jwt_config['private_key'], config.jwt_config['algorithm'], config.jwt_config['token_ttl'])
+        self.client = self.get_mqtt_client(config.google_cloud_config['project_id'], config.google_cloud_config['cloud_region'], config.google_cloud_config['registry_id'], config.google_cloud_config['device_id'], self.jwt)
 
-    # Epoch_offset is needed because micropython epoch is 2000-1-1 and unix is 1970-1-1. Adding 946684800 (30 years)
-    epoch_offset = 946684800
-    claims = {
-            # The time that the token was issued at
-            'iat': utime.time() + epoch_offset,
-            # The time the token expires.
-            'exp': utime.time() + epoch_offset + token_ttl,
-            # The audience field should always be set to the GCP project id.
-            'aud': project_id
-    }
+    def get_client(self):
+        return(self.client)
 
-    #This only supports RS256 at this time.
-    header = { "alg": algorithm, "typ": "JWT" }
-    content = b42_urlsafe_encode(ujson.dumps(header).encode('utf-8'))
-    content = content + '.' + b42_urlsafe_encode(ujson.dumps(claims).encode('utf-8'))
-    signature = b42_urlsafe_encode(rsa.sign(content,private_key,'SHA-256'))
-    return content+ '.' + signature #signed JWT
+    def create_jwt(self, project_id, private_key, algorithm, token_ttl):
+        print("Creating JWT...")
+        private_key = rsa.PrivateKey(*private_key)
 
-def get_mqtt_client(project_id, cloud_region, registry_id, device_id, jwt):
-    """Create our MQTT client. The client_id is a unique string that identifies
-    this device. For Google Cloud IoT Core, it must be in the format below."""
-    client_id = 'projects/{}/locations/{}/registries/{}/devices/{}'.format(project_id, cloud_region, registry_id, device_id)
-    print('Sending message with password {}'.format(jwt))
-    client = MQTTClient(client_id.encode('utf-8'),server=config.google_cloud_config['mqtt_bridge_hostname'],port=config.google_cloud_config['mqtt_bridge_port'],user=b'ignored',password=jwt.encode('utf-8'),ssl=True)
-    client.set_callback(on_message)
-    client.connect()
-    client.subscribe('/devices/{}/config'.format(device_id), 1)
-    client.subscribe('/devices/{}/commands/#'.format(device_id), 1)
-    return client
+        # Epoch_offset is needed because micropython epoch is 2000-1-1 and unix is 1970-1-1. Adding 946684800 (30 years)
+        epoch_offset = 946684800
+        claims = {
+                # The time that the token was issued at
+                'iat': utime.time() + epoch_offset,
+                # The time the token expires.
+                'exp': utime.time() + epoch_offset + token_ttl,
+                # The audience field should always be set to the GCP project id.
+                'aud': project_id
+        }
+
+        #This only supports RS256 at this time.
+        header = { "alg": algorithm, "typ": "JWT" }
+        content = b42_urlsafe_encode(ujson.dumps(header).encode('utf-8'))
+        content = content + '.' + b42_urlsafe_encode(ujson.dumps(claims).encode('utf-8'))
+        signature = b42_urlsafe_encode(rsa.sign(content,private_key,'SHA-256'))
+        return content+ '.' + signature #signed JWT
+
+    def get_mqtt_client(self, project_id, cloud_region, registry_id, device_id, jwt):
+        """Create our MQTT client. The client_id is a unique string that identifies
+        this device. For Google Cloud IoT Core, it must be in the format below."""
+        client_id = 'projects/{}/locations/{}/registries/{}/devices/{}'.format(project_id, cloud_region, registry_id, device_id)
+        print('Sending message with password {}'.format(jwt))
+        client = MQTTClient(client_id.encode('utf-8'),server=config.google_cloud_config['mqtt_bridge_hostname'],port=config.google_cloud_config['mqtt_bridge_port'],user=b'ignored',password=jwt.encode('utf-8'),ssl=True)
+        client.set_callback(on_message)
+        client.connect()
+        client.subscribe('/devices/{}/config'.format(device_id), 1)
+        client.subscribe('/devices/{}/commands/#'.format(device_id), 1)
+        return client
 
 wlan.do_connect()
 #Need to be connected to the internet before setting the local RTC.
 set_time()
 
-jwt = create_jwt(config.google_cloud_config['project_id'], config.jwt_config['private_key'], config.jwt_config['algorithm'], config.jwt_config['token_ttl'])
-client = get_mqtt_client(config.google_cloud_config['project_id'], config.google_cloud_config['cloud_region'], config.google_cloud_config['registry_id'], config.google_cloud_config['device_id'], jwt)
+m = MQTTgcloud()
+client = m.get_client()
 
 while True:
     message = {
