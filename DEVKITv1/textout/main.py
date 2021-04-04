17d0e7edbae734e851215e2d6c2e99dc0b1ceac6
cloudymike <mikael@cloudyforecast.com>
2021-04-04 13:17:43 -0700

    usetempctrl

---
+++ DEVKITv1/textout/main.py
@@ -0,0 +1,11 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+import textout
+import time
+
+t = textout.textout()
+while True:
+    t.text('Meaning of life?')
+    time.sleep(1)
+    t.text(42)
+    time.sleep(2)
