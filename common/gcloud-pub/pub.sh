e763f53fc846b3af6c8edc432fa94c3df2f244f2
cloudymike <mikael@cloudyforecast.com>
2021-04-04 21:04:57 -0700

    simple

--- common/gcloud-pub/pub.sh
+++ common/gcloud-pub/pub.sh
@@ -2,7 +2,7 @@
 #gcloud pubsub topics publish any --message="hello"
 
 gcloud iot devices commands send \
-    --command-data='hello' \
+    --command-data=$1 \
     --region=us-central1  \
     --registry=tempctrl \
     --device=esp32tempctrl
