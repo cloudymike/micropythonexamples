4041721ae663a02b6109f8ce099e3206c322a135
cloudymike <mikael@cloudyforecast.com>
2022-11-29 21:21:51 -0800

    Run without display

--- DEVKITv1/textout/RUN.sh
+++ DEVKITv1/textout/RUN.sh
@@ -3,8 +3,9 @@ PORT='/dev/ttyUSB0'
 PUSHCMD="ampy --port $PORT put "
 
 $PUSHCMD ../oled/ssd1306.py
+$PUSHCMD ../oled/gfx.py
 $PUSHCMD textout.py
 $PUSHCMD main.py
 
 echo "Resetting board"
-sudo timeout 2  ampy --port /dev/ttyUSB0 run ../reset/reset.py
+timeout 2  ampy --port /dev/ttyUSB0 run ../reset/reset.py
