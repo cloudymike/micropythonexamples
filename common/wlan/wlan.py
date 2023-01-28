2670a3c91e06c26e35fd74a5598f11df1e66acb6
cloudymike <mikael@cloudyforecast.com>
2023-01-27 19:27:43 -0800

    Test working for v19.1

--- common/wlan/wlan.py
+++ common/wlan/wlan.py
@@ -11,12 +11,16 @@ import time
 def do_connect(hostname='micropythonexamples'):
     nic = network.WLAN(network.STA_IF)
     nic.active(True)
+    # Add a delay for this issue in 19.1 https://github.com/micropython/micropython/issues/9236
+    time.sleep_ms(100)
     nic.config(dhcp_hostname=hostname)
+    time.sleep_ms(100)
+    nic.connect(wlanconfig.ESSID,wlanconfig.PASSWORD)
+    time.sleep_ms(100)
     if not nic.isconnected():
         print('connecting to network...')
         while not nic.isconnected():
-            nic.connect(wlanconfig.ESSID,wlanconfig.PASSWORD)
-            time.sleep_ms(500)
+            time.sleep_ms(100)
 
     print('network config:', nic.ifconfig())
     return(nic)
--- tools/loadmicropython.sh
+++ tools/loadmicropython.sh
@@ -8,8 +8,8 @@ PACKAGE=esp32-idf3-20190529-v1.11.bin
 #PACKAGE=esp32-idf3-20210130-unstable-v1.13-305-gb8f4c623f.bin
 #PACKAGE=esp32-20220117-v1.18.bin
 # Old: MicroPython v1.11 on 2019-05-29; ESP32 module with ESP32
-PACKAGE=esp32-idf3-20190529-v1.11.bin
-#PACKAGE=esp32-20220618-v1.19.1.bin
+#PACKAGE=esp32-idf3-20190529-v1.11.bin
+PACKAGE=esp32-20220618-v1.19.1.bin
 
 if [ ! -f $PACKAGE ]
 then
@@ -21,5 +21,5 @@ source venv/bin/activate
 pip install  -r requirements.txt
 
 timeout 1  ampy --port /dev/ttyUSB0 run reset.py
-esptool.py --port /dev/ttyUSB0 erase_flash
+esptool.py --chip esp32 --port /dev/ttyUSB0 erase_flash
 esptool.py --chip esp32 --port /dev/ttyUSB0 write_flash -z 0x1000 ./$PACKAGE
