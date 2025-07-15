5b022d00f53214ee329989c470c75168fb41cff5
cloudymike <mikael@cloudyforecast.com>
2025-07-14 19:51:51 -0700

    textout

--- ESP32C3/textout/textout.py
+++ ESP32C3/textout/textout.py
@@ -13,17 +13,16 @@ class textout:
         # Check if display is there.
         # If not, keep running but just output text in log
         try:
+
+            # ESP32C3 Pin assignment
+            i2c = I2C(-1, scl=Pin(7), sda=Pin(6))
+            
             # ESP32 Pin assignment
-            i2c = I2C(-1, scl=Pin(22), sda=Pin(21))
+            #i2c = I2C(-1, scl=Pin(22), sda=Pin(21))
 
             # ESP8266 Pin assignment
             #i2c = I2C(-1, scl=Pin(5), sda=Pin(24))
 
-            # Reset OLED
-            oledReset=Pin(16, Pin.OUT)
-            oledReset.value(0)
-            sleep_ms(500)
-            oledReset.value(1)
 
             self.oled_width = 128
             self.oled_height = 64
